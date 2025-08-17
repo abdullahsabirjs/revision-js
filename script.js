3f7cad6175f54cc181df05ed687f6a4be9276c39
abdullahsabirjs <mailk1abdullah892@gmail.com>
2025-08-17 16:04:31 +0500

    js added

---
+++ script.js
@@ -0,0 +1,157 @@
+// pracrice var let const
+
+// var a = "samama" ;
+// console.log(a);
+
+// let b = "market";
+// console.log(b);
+
+// const country = "pakistan";
+// console.log(country);
+
+
+// if (true){
+//     var name = "abdullah";
+// }
+
+// console.log(name)
+
+// if (true){
+//     let naam = "abdullah";
+// }
+// console.log(naam);
+
+// if (true) {
+//     const country = "pakistan";
+// }
+// console.log(country);
+
+// console.log(a);
+// var a = "sabir";
+
+// console.log(b);
+// let b = "bol jani"; 
+
+// console.log(c);
+// const c = "jj47";
+
+
+
+
+
+// Data type
+
+// let name = "abdullah";
+// let messege = 'hello world';
+// let great = `name ${name}`;
+
+// console.log(name, messege, great);
+
+
+// let age = "20";
+// let price = "99.99"
+
+// console.log(age,price);
+
+// let isStudent =  true;
+// let hasJob = false ;
+
+// console.log(isStudent, hasJob);
+
+// let car = null;
+// console.log(car);
+
+// let x;
+// console.log(x);
+
+// let id1 = Symbol("id");
+// let id2 = Symbol("id");
+// console.log(id1 === id2);
+
+// let fruit = "apple";
+// console.log(typeof fruit);
+
+// let price = 250;
+// console.log(typeof price);
+
+// let emptyValue = null;
+// console.log(typeof emptyValue);
+
+
+
+// Type Conversion & Coercion in JavaScript
+
+// let num1 = Number("123");
+// console.log(num1, typeof num1);
+
+// let num2 = String(123);
+// console.log(num2, typeof num2);
+
+// console.log(Number(true)); 
+// console.log(Number(false));
+// console.log(Boolean("0"));
+
+
+// console.log("5" + 2);
+// console.log("5" - 2);
+// console.log("5" * 2);
+// console.log(1 + true);
+// console.log(1 + false);
+
+
+
+// Operators (Arithmetic, Comparison, Logical, Ternary)
+
+// let num1 = 5;
+// let num2 = "3";
+// let num3 = "5";
+// let val1 = "addi"
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 + num3);
+// console.log(num2 + val1);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 / num3);
+// console.log(num1 ** num2);
+// console.log(2**3);
+// console.log(++num1);
+// console.log(num1++);
+// console.log(--num2);
+
+// Comparison Operators
+
+// console.log(num1 == num3);
+// console.log(num1 === num3);
+// console.log(num1 != num3);
+// console.log(num1 !== num3);
+// console.log(num1 > num2);
+// console.log(num1 <= num3);
+
+// Logical Operators
+
+// let age = 20;
+
+// console.log(age > 18 && age < 25);
+// console.log(age > 25 || age === 20);
+
+// Ternary Operator
+// condition ? valueIfTrue : valueIfFalse
+
+// let result = (age <= 19) ? "adult" : "minor";
+// console.log(result);
+// let marks = 40;
+// let status = (marks <= 30) ? "Pass" : "Fail";
+// console.log(status);
+
+
+
+// Conditionals (if, else, switch)
+
+// let age = 18;
+
+// if (age >=18){
+//     console.log(`your age is ${age} you are eligible to vote`);
+    
+}
